c94515db2e5351bef4cdd1ff4dd7d26afe6c3e3c
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2022-03-08 22:54:59 +0100

    fixing memory leak

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -22,6 +22,12 @@ export class ModalService {
     console.log(this.modals);
   }
 
+  unregister(id:string) {
+    this.modals = this.modals.filter(
+      element => element.id != id
+    )
+  }
+
   isModalOpen(id: string): boolean {
     return !!this.modals.find(element => element.id === id)?.visible
   }
