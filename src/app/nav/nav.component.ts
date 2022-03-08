5c445150eaaf31d58c3128108bc392da9f879fc4
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2022-03-08 05:51:57 +0100

    open the modal

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  ModalService
+} from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +13,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modal: ModalService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  openModal($event: Event) {
+    $event.preventDefault;
+
+    this.modal.toggleModal();
   }
 
 }
