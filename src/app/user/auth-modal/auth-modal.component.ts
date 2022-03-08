c6e0a666f9a3665ee676f7b483bdf4abdc57d3ee
xTbeniaminTx <beniamin.tolan@outlook.com>
2022-03-08 13:17:01 +0100

    test nat

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalService} from "../../services/modal.service";
 
 @Component({
   selector: 'app-auth-modal',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(modal: ModalService) { }
 
   ngOnInit(): void {
   }
