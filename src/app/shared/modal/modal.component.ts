5f6533f2083f062e6d02ef77e10fec7e0ec765d1
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2022-03-08 23:15:38 +0100

    css problems

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,5 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import { ModalService } from 'src/app/services/modal.service';
+import {
+  Component,
+  Input,
+  OnInit,
+  ElementRef
+} from '@angular/core';
+import {
+  ModalService
+} from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
@@ -8,13 +15,14 @@ import { ModalService } from 'src/app/services/modal.service';
   // providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
-  @Input() modalID="";
+  @Input() modalID = "";
 
-  constructor(public modal: ModalService) {
-
+  constructor(public modal: ModalService, public el: ElementRef) {
+    
   }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   closeModal($event: Event) {
