b36948abfb9ca999e3f0444d91adb7e84a914b7e
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2022-03-08 05:40:44 +0100

    service injection 3 ways

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -4,12 +4,13 @@ import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
 
   constructor(public modal: ModalService) { 
-    console.log(this.modal.visible)
+   
   }
 
   ngOnInit(): void {
